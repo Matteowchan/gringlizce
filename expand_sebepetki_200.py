d39a27e6d674a39d667ec9bb02059d5bdee952c6
Matteowchan <mertatasal@gmail.com>
2026-06-04 14:39:29 +0000

    Sebep-Etki ve Onleme: 100 -> 200 kelime

---
+++ expand_sebepetki_200.py
@@ -0,0 +1,24 @@
+import re
+path = 'kelime-bankasi.html'
+with open(path, encoding='utf-8') as f:
+    html = f.read()
+
+html = html.replace(
+    '<div><span class="s-num">2.200+</span><span class="s-lbl">Kelime</span></div>',
+    '<div><span class="s-num">2.300+</span><span class="s-lbl">Kelime</span></div>'
+)
+print('[+] Hero: 2.200+ -> 2.300+ Kelime')
+
+pat = re.compile(
+    r'(class="qb-hub-card cat-kb-sebep-etki-onleme".*?<span class="m-num">)100(</span><span class="m-lbl">Kelime</span>)',
+    re.DOTALL
+)
+new_html, n = pat.subn(r'\g<1>200\g<2>', html)
+if n:
+    html = new_html
+    print(f'[+] Sebep-Etki card: 100 -> 200')
+else:
+    print('[!] Card etiketi bulunamadi')
+
+with open(path, 'w', encoding='utf-8') as f:
+    f.write(html)
